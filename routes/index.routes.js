a5a85590cb4716ec4566b8e662cfd636bd2c0c5e
estebanba <esteban.ba@gmail.com>
2022-06-09 11:37:41 +0200

    merge with style

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,8 @@
 const router = require("express").Router();
 
-
-
 /* GET home page */
 router.get("/", (req, res, next) => {
-<<<<<<< HEAD
-  res.render("index", {userInSession: req.session.currentUser});
-=======
   res.render("index", { userInSession: req.session.currentUser });
->>>>>>> style
 });
 
 module.exports = router;
